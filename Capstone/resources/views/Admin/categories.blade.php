12d026af64ce4ae23cc23f9cb9be43ae31941f6f
josejose321 <57382439+josejose321@users.noreply.github.com>
2022-04-30 20:39:53 +0800

    category view

--- Capstone/resources/views/Admin/categories.blade.php
+++ Capstone/resources/views/Admin/categories.blade.php
@@ -5,18 +5,18 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class=" h2 font-weight-bold">SCHOALARSHIP CATEGORIES</div>
+        <div class="h2 font-weight-bold">SCHOLARSHIP AND GRANTS CATEGORIES</div>
         <div class="card shadow">
             <div class="card-header py-3">
-                <div class="h3 font-weight-bold">SCHOLARSHIP CATEGORIES</div>
+                <div class="h2 font-weight-bold">Available Categories</div>
+                <button type="button" class="btn btn-lg btn-secondary float-right" id="categoryBtn"><i
+                    class="fa fa-plus"></i><span>Add new</span></button>
             </div>
+            
             <div class="card-body" style="font-size: 14px;">
                 <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+                    
                     <table class="table my-0" id="dataTable">
-                        <div class="col mb-3 w-25 float-right">
-                            <button type="button" class="btn btn-secondary" id="categoryBtn"><i
-                                    class="fa fa-plus"></i><span>Add new</span></button>
-                        </div>
                         <thead>
                             <tr style="background: var(--bs-red);color: var(--bs-body-bg);font-size: 15px;">
                                 <th>Category No</th>
@@ -37,7 +37,7 @@
                                     <th>{{ $category->field_team }}</th>
                                     <th>{{ $category->memberCount }}</th>
                                     <td><button class="btn-lg btn-secondary" type="button" data-toggle="modal"
-                                        data-target="#admin_viewCategory-{{ $category->categoryNo }}">
+                                        data-target="#viewCategory-{{ $category->categoryNo }}">
                                         <i class="fa fa-pencil"></i></button>
                                     <button class="btn-lg btn-danger" type="button">
                                         <i class="fa fa-trash-o"></i></button>
@@ -50,11 +50,10 @@
                         {!! $categories->links() !!}
                     </center>
                 </div>
-                <div class="row">
-                </div>
             </div>
         </div>
     </div>
     @include('modals.addCategory')
+    @include('modals.viewCategory')
 
 @endsection
