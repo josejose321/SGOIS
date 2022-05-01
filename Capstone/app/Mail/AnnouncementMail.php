b56a22828292266f8d1a7f0eaa750a9138a7183d
josejose321 <57382439+josejose321@users.noreply.github.com>
2022-05-01 15:07:52 +0800

    announcement email script

---
+++ Capstone/app/Mail/AnnouncementMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AnnouncementMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    private $announcement;
+    public function __construct($announcement)
+    {
+        //
+        $this->announcement = $announcement;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Announcement')
+        ->markdown('emails.announcementMail')
+        ->with($this->announcement);
+    }
+}
